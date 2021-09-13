28c9115374f5a67fca91cd793d01a4941fa160ca
Sneha Mathadawar <mathadawar01@gmail.com>
2021-09-13 12:04:46 +0530

    code has been changed

--- Simple.java
+++ Simple.java
@@ -1,7 +1,8 @@
+//Greeting program 
 public class Simple
 {
    public static void main(String[] args)
    {
-      System.out.println("Hello EveryOne");
+      System.out.println("Hello folks");
    }
 }
